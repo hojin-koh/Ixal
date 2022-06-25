115913e27e66352e8c465515f21994b2452516ab
Hojin Koh <hojin-koh@users.noreply.github.com>
2022-06-25 13:58:04 +0800

    ??????

--- Ixal/unit.py
+++ Ixal/unit.py
@@ -7,9 +7,12 @@ from plumbum import local
 from logzero import setup_logger
 logger = setup_logger('Ixal')
 
+eik.config.metapath = ".meta"
+
 
 class DownloadTask(eik.Task):
     url = lg.Parameter()
+    pathDownload = lg.Parameter(".build/.cache")
     cmd = lg.ListParameter(significant=False, default=("curl", "-qfLC", "-", "--retry", "5", "--retry-delay", "5", "-o", "{1}", "{0}"))
 
     def parseFileName(self):
@@ -19,13 +22,18 @@ class DownloadTask(eik.Task):
         return p.name
 
     def generates(self):
-        return eik.MetaTarget(self, self.parseFileName())
+        return eik.MetaTarget(self, Path(pathDownload) / self.parseFileName())
 
     def run(self):
         with self.output().pathWrite() as fw:
             args = [s.format(self.url, fw) for s in self.cmd[1:]]
             self.ex(local[self.cmd[0]][args])
 
+class ExtractTask(eik.Task):
+    src = eik.TaskParameter()
+    pathDownload = lg.Parameter(".build/.cache")
+    cmdTar = lg.ListParameter(significant=False, default=("tar", "xf", "{0}", "-C", "{1}"))
+    pass
 
 class Unit:
     # Expected to get a plumbum object
@@ -34,6 +42,14 @@ class Unit:
         chain & FG
 
     def make(self):
-        eik.run((DownloadTask(self.src),))
+        urls = self.src
+        if isinstance(urls, str):
+            urls = (urls,)
+
+        aTaskSource = []
+        for f in 
+            
+            eik.run((DownloadTask(self.src),))
+        eik.run((ATask(),))
         print("MAKE")
 
