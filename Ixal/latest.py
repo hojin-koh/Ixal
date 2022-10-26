f4603aacfbc0de0fe85eb1932a969c5a010c0714
Hojin Koh <hojin-koh@users.noreply.github.com>
2022-10-26 22:21:21 +0800

    Fix a missing "elif" in figuring out what backend to use to check update

--- Ixal/latest.py
+++ Ixal/latest.py
@@ -26,7 +26,7 @@ def isLatest(spec, ver):
         res = requests.get('https://archlinux.org/packages/{}'.format(repo))
         html = res.content.decode('utf-8')
         latest = re.search(R'<h2>[^< ]+ ([^<-]+)-[^<]*</h2>', html)[1]
-    if src == 'portapps':
+    elif src == 'portapps':
         res = requests.get('https://portableapps.com/apps/{}'.format(repo))
         html = res.content.decode('utf-8')
         latest = re.search(R'<p class=[^>]+>Version ([^ ]+) ', html)[1]
